3f46a0fc6646f9f4cad691e3065cfe71a369456e
2104499700 <elcio.abrahao@viavarejo.com.br>
2020-11-18 20:49:34 -0300

    Implementação de sensor de biometria Implementação de sensor de movvimento (SHAKE)

--- app/src/main/java/br/usjt/ucsist/chatfirebase/ui/MainActivity.kt
+++ app/src/main/java/br/usjt/ucsist/chatfirebase/ui/MainActivity.kt
@@ -5,20 +5,81 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import br.usjt.ucsist.chatfirebase.R
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.Executor
 
 
 class MainActivity : AppCompatActivity() {
 
     private var mAuth: FirebaseAuth? = null
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        buttonBiometria.isEnabled = false
+        val biometricManager = BiometricManager.from(this)
+        when (biometricManager.canAuthenticate()) {
+            BiometricManager.BIOMETRIC_SUCCESS ->
+                habilitarBotaoBiometria()
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                Toast.makeText(this,"Erro ao tentar autenticar com biometria. Use login e senha!",Toast.LENGTH_SHORT).show()
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                Toast.makeText(this,"Não há biometrica disponível. Use login e senha!",Toast.LENGTH_SHORT).show()
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->
+                Toast.makeText(this,"Para usar biometrica faça pelo menos um cadastro ou use login e senha!",Toast.LENGTH_SHORT).show()
+        }
         mAuth = FirebaseAuth.getInstance()
+
+        buttonBiometria.setOnClickListener(View.OnClickListener {
+          _ ->
+            executor =
+                    ContextCompat.getMainExecutor(this)
+            biometricPrompt = BiometricPrompt(this, executor,
+                    object : BiometricPrompt.AuthenticationCallback() {
+                        override fun onAuthenticationError(errorCode: Int,
+                                                           errString: CharSequence) {
+                            super.onAuthenticationError(errorCode, errString)
+
+                            Toast.makeText(applicationContext,
+                                    "AErro ao tentar autenticar com biometria. Use login e senha!", Toast.LENGTH_SHORT)
+                                    .show()
+                        }
+
+                        override fun onAuthenticationSucceeded(
+                                result: BiometricPrompt.AuthenticationResult) {
+                            super.onAuthenticationSucceeded(result)
+                            val intent = Intent(this@MainActivity,ChatActivity::class.java)
+                            startActivity(intent)
+                        }
+
+                        override fun onAuthenticationFailed() {
+                            super.onAuthenticationFailed()
+                            Toast.makeText(applicationContext, "A autenticação biometrica falhou. Use login e senha!",
+                                    Toast.LENGTH_SHORT)
+                                    .show()
+                        }
+                    })
+
+            promptInfo = BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("Autenticação Biometrica do Chat")
+                    .setSubtitle("Logue com sua autenticação biométrica")
+                    .setNegativeButtonText("Cancelar")
+                    .build()
+            biometricPrompt.authenticate(promptInfo)
+        })
+    }
+
+    fun habilitarBotaoBiometria(){
+        buttonBiometria.isEnabled = true
+        buttonBiometria.background = getDrawable(R.drawable.ic_fingerprint)
     }
 
     fun fazerLogin(view: View?) {
--- app/src/main/java/br/usjt/ucsist/chatfirebase/ui/MapsActivity.kt
+++ app/src/main/java/br/usjt/ucsist/chatfirebase/ui/MapsActivity.kt
@@ -1,5 +1,6 @@
 package br.usjt.ucsist.chatfirebase.ui
 
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.usjt.ucsist.chatfirebase.R
@@ -10,8 +11,12 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.squareup.seismic.ShakeDetector
+import com.squareup.seismic.ShakeDetector.SENSITIVITY_HARD
+import com.squareup.seismic.ShakeDetector.SENSITIVITY_LIGHT
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, ShakeDetector.Listener {
 
     private lateinit var mMap: GoogleMap
     private var mensagem: Mensagem? = null
@@ -24,6 +29,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        val sd = ShakeDetector(this)
+        sd.setSensitivity(SENSITIVITY_LIGHT)
+        sd.start(sensorManager)
 
     }
 
@@ -41,7 +51,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         if(mensagem != null){
             val userLocation = LatLng(mensagem!!.latitude!!, mensagem!!.longitude!!)
-            mMap.addMarker(MarkerOptions().position(userLocation).title("Localização de "+mensagem!!.usuario))
+            mMap.addMarker(
+                MarkerOptions().position(userLocation).title("Localização de " + mensagem!!.usuario)
+            )
             mMap.animateCamera(
                 CameraUpdateFactory.newLatLngZoom(
                     userLocation, 16.0f
@@ -50,5 +62,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         }
     }
+
+    override fun hearShake() {
+        finish()
+    }
+
 
 }
